bb01bd9eda8cc67047ce069d3f8df576d2192f8a
PavelBezzub <48061509+PavelBezzub@users.noreply.github.com>
2019-03-26 13:38:51 +0300

    Update matrix.h

--- 02/matrix.h
+++ 02/matrix.h
@@ -1,3 +1,4 @@
+#include <new>
 class Matrix
 {
 	class proxy
@@ -5,88 +6,69 @@ class Matrix
 	public:
 		proxy() :arr(NULL), size(0) {}
 		~proxy() { delete[](arr); }
-		void init(int columns);
-		proxy(proxy& a)
-		{
-			size = a.size;
-			arr = new int[a.size];
-			for (int i = 0; i < size; i++) {
-				arr[i] = a.arr[i];
-			}
-		}
-		void copy(proxy &a) {
-			arr = new int[a.size];
-			for (int i = 0; i < a.size; i++) {
-				arr[i] = a.arr[i];
-			}
-			size = a.size;
+		proxy(int columns)
+		{ 
+			arr = new int[columns];
+			size = columns;
 		}
 		const int& operator[](int index) const;
 		int& operator[](int index);
 	private:
-		int size;
+		size_t size;
 		int *arr;
 	};
 public:
-	Matrix(int row, int columns);
+	Matrix(int  row, int columns);
 	~Matrix();
-	Matrix  operator*=(int x);
-	const bool operator==(const Matrix& other);
-	const bool operator!=(const Matrix& other);
+	Matrix  &operator*=(int x);
+	bool operator==(const Matrix& other);
+	bool operator!=(const Matrix& other);
 	const proxy& operator[](int index) const;
 	proxy& operator[](int index);
-	Matrix(Matrix& a) {
-		row = a.row;
-		M_matrix = new proxy[a.row];
-		for (int i = 0; i < a.row; i++) {
-			M_matrix[i].copy(a.M_matrix[i]);
-		}
-
-	}
 	int getRows();
 	int getColumns();
 
 private:
 	proxy *M_matrix;
-	int row;
-	int columns;
+	size_t row;
+	size_t columns;
 };
-void Matrix::proxy::init(int columns)
-{
-	arr = new int[columns];
-	size = columns;
-}
+
 const int& Matrix::proxy::operator[](int index) const
-{   
+{
 	if (index >= size) throw std::out_of_range("");
 	return arr[index];
 }
-int& Matrix::proxy::operator[](int index)  
+int& Matrix::proxy::operator[](int index)
 {
 	if (index >= size) throw std::out_of_range("");
 	return arr[index];
 }
 
-const Matrix::proxy& Matrix::operator[](int index) const{
+const Matrix::proxy& Matrix::operator[](int index) const {
 	if (index >= row) throw std::out_of_range("");
 	return M_matrix[index];
 }
-Matrix::proxy& Matrix::operator[](int index)  {
+Matrix::proxy& Matrix::operator[](int index) {
 	if (index >= row) throw std::out_of_range("");
 	return M_matrix[index];
 }
 
-Matrix::Matrix(int rows, int column): row(rows), columns(column)
-{   
-	M_matrix = new proxy[row];
+Matrix::Matrix(int rows, int column) : row(rows), columns(column)
+{
+	M_matrix = static_cast<proxy*>(operator new[](row * sizeof(proxy)));
 	for (int i = 0; i < row; i++)
 	{
-		M_matrix[i].init(columns);
+		new(M_matrix + i) proxy(columns);
 	}
 }
 Matrix::~Matrix()
-{
-	delete[](M_matrix);
+{   
+	for (int i = 0; i < row; i++)
+	{
+		M_matrix[i].~proxy();
+	}
+	operator delete[](M_matrix);
 }
 
 int Matrix::getRows() {
@@ -94,22 +76,20 @@ int Matrix::getRows() {
 }
 
 int Matrix::getColumns() {
-	return columns; 
+	return columns;
 }
 
-Matrix  Matrix::operator*=(int x) {
-	for (int i =0; i < row;i++ )
+Matrix  &Matrix::operator*=(int x) {
+	for (int i = 0; i < row; i++)
 		for (int j = 0; j < columns; j++)
 		{
 			M_matrix[i][j] = M_matrix[i][j] * x;
 		}
-	
-	Matrix M2(*this);
-	return M2;
+	return *this;
 }
 
-const bool Matrix::operator==(const Matrix& other) {
-    if ( (columns != other.columns) || (row != other.row)) return false;
+bool Matrix::operator==(const Matrix& other) {
+	if ((columns != other.columns) || (row != other.row)) return false;
 	for (int i = 0; i < row; i++) {
 		for (int j = 0; j < columns; j++)
 		{
@@ -118,6 +98,6 @@ const bool Matrix::operator==(const Matrix& other) {
 	}
 	return true;
 }
-const bool Matrix::operator!=(const Matrix& other) {
-	return !(*this==other);
+bool Matrix::operator!=(const Matrix& other) {
+	return !(*this == other);
 }
